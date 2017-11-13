a5361f59dfb142afa8c7ec8246939ca5939fde03
beSyl <32633621+beSyl@users.noreply.github.com>
2017-11-13 03:39:53 +0100

    [DEBUG] Erreur dans l'essai de compréhension de listes pour déterminer 'walls' et 'road' : boucle while à la place.

--- constantes.py
+++ constantes.py
@@ -1,7 +1,7 @@
 #VARIABLES-----
 list_labyrinth=[]#Toutes les cases, numérotées de 0 à ALL_SQUARES-1.
-walls=[x for x in list_labyrinth if x=="X"]
-road=[x for x in list_labyrinth if x==" "]
+walls=[]
+road=[]
 objects=[]#Cases "objets" créées de manière aléatoire à partir de la liste 'road' (NB : ces cases sont à la fois dans les listes 'objects' et 'road')
 
 
