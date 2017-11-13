deb2964fc9d072626f1af08cba5f63349bedad41
beSyl <32633621+beSyl@users.noreply.github.com>
2017-11-13 02:22:37 +0100

    [ADD]Définition du numéro de ligne et le colonne de chaque case dans Hero.move(). [ADD]Fichiers 'constantes' et 'classes'.

---
+++ constantes.py
@@ -0,0 +1,7 @@
+#VARIABLES-----
+walls=[]#Uniquement les cases "murs"
+road=[]#Uniquement les cases "chemin"
+list_labyrinth=[]#Toutes les cases, numérotées de 0 à ALL_SQUARES-1.
+objects=[]#Cases "objets" créées de manière aléatoire à partir de la liste 'road' (NB : ces cases sont à la fois dans les listes 'objects' et 'road')
+
+#IMAGES-----
--- jeu.py
+++ jeu.py
@@ -1,118 +1,31 @@
 import random
-
-#VARIABLES-----
-walls=[]#uniquement les cases "murs"
-road=[]#uniquement les cases "chemin"
-list_labyrinth=[]#toutes les cases, numérotées de 0 à numberOfSquares-1.
-objects=[]#cases "objets" créées de manière aléatoire à partir de la liste 'road' (NB : ces cases sont à la fois dans les listes 'objects' et 'road')
-end = []
-
-#CLASSES-----
-#STRUCTURE DU LABYRINTHE.
-class Labyrinth:
-    HEIGHT=15 #hauteur en cases.
-    WIDTH=15 #largeur en cases.
-    numberOfSquares = HEIGHT*WIDTH #nombre de cases du labyrinthe.
-
-    def __init__(self):#Transformer le fichier labyrinthe en liste (i.e. une liste des cases du plateau de jeu).
-        with open('labyrinth.txt', 'r') as f:
-            file = f.read()
-        for element in file: #tant qu'il y a un élément dans le fichier
-            if element!="\n": #sauf s'il s'agit d'un saut de ligne
-                list_labyrinth.append(element)
+from classes import *
 
 
-#OBJETS A RAMASSER.
-class Object:
-    objects_counter = 0 #compteur d'instances de la classe.
-    def __init__(self, name, position):
-        self.name = name #nom de l'objet
-        self.position = position #numéro de case par défaut
-        Object.objects_counter += 1 #incrément le compteur d'instance de classe Object.
+#GAME-----
+labyrinth = Labyrinth() #Initialiser le plateau de jeu.
+macGyver = Hero() #Initialiser le héros.
 
-
-#PERSONNAGE DU JEU.
-class Hero:
-    def __init__(self):
-        self.position = 0 #emplacement dans le labyrinthe exprimé en numéro de case, case 0 au début du jeu.
-        self.objectsCounter = 0 #objets ramassée, aucun par défaut au début du jeu.
-        self.picture = "images/hero.jpg" #ACTUALISER / CORRIGER
-
-    def move(self, direction):
-        movement=0
-        
-        if direction == "right":
-            movement = 1
-        elif direction == "left":
-            movement = -1
-        elif direction == "up":
-            movement = -Labyrinth.WIDTH
-        elif direction == "down":
-            movement = Labyrinth.WIDTH
-
-        if self.position in objects:
-            print("Un objet trouvé !")
-            self.objectsCounter+=1
-            a=self.position
-            objects.remove(a)
-                
-        #Si le déplacement envisagé conduit à une case "chemin" (ou une case "objet" figurant à la fois dans les listes 'road' et 'objects').
-        #Implicitement, le déplacement est >=0 et <numberOfSquares).
-        if self.position+movement in road:
-            self.position+=movement
-            #Si le déplacement conduit à une case "objet", le personnage le ramasse.
-            #L'objet est supprimé de la list 'objects' MAIS sa case est toujours présente dans la liste 'road' dont elle est issue.
-
-        elif self.position == Labyrinth.numberOfSquares:
-            if self.objectsCounter == 3:
-                print("MacGyver a trouvé tous les objets : il a gagné.")
-            else:
-                print("MacGyver n'a pas trouvé tous les objets : il a perdu.")
-        #Si le déplacement conduit à une case "mur" ou sort des limites du plateau de jeu (résultat négatif ou supérieur au numéro de la case de fin), aucun mouvement.
-        else: 
-            movement=0
-        
-        print(self.position)
-
-
-#JEU-----
-labyrinth = Labyrinth() #initialiser le plateau de jeu.
-macGyver = Hero()
-
-#Créer une liste d'ID des murs à partir du fichier labyrinthe (i.e. leur emplacement dans la liste des cases)
-#Créer une liste d'ID des passages à partir du fichier labyrinthe.
 i=0
-while i<Labyrinth.numberOfSquares:
-    if list_labyrinth[i] != "\n":
-        if list_labyrinth[i]=="X":
-            walls.append(i)
-        elif list_labyrinth[i]==" ":
-            road.append(i)
-        elif list_labyrinth[i] == "e":
-            end.append(i)
-    i+=1
-#Créer les objets, en position '0' par défaut.
-objects.append(Object("tube", 0))
-objects.append(Object("aiguille", 0))
-objects.append(Object("ether", 0))
-
-#Créer la position de chaque objet à ramasser, à partir de la liste 'road'.
-i=0
-while i<Object.objects_counter: #on crée autant de chiffres qu'il existe d'instances de la classe Object.
-    number=random.randint(1, Labyrinth.numberOfSquares-1) #on exclut la première et la dernière case, soit le départ et l'arrivée du jeu, pour positionner les objets à ramasser.
-    if number in road:
-        objects[i].position = number
+while i<3: #Créer autant de chiffres qu'il existe d'objets.
+    number=random.randint(1, road[-1]) #Exclure la première et la dernière case, soit le départ et l'arrivée du jeu, pour positionner les objets à ramasser.
+    if number in road and number not in objects: #S'assurer que la position correspond à une case 'chemin' et qu'elle ne soit pas déjà associée à un objet créé précédemment dans 'objects'.
+        objects.append(number)
         i+=1
 
+#Déterminer les objets à partir des positions créées de manière aléatoire dans 'objects'.
+tube = objects[0]
+needle = objects[1]
+ether = objects[2]
 
 #VERIFICATION-----
 print("Cases 'chemin' : " + str(road))     
 print("Cases 'mur' : " + str(walls))
-print("Case 'fin' : " + str(end))
-
-print("Objet " + str(objects[0].name) + " en case " + str(objects[0].position))
-print("Objet " + str(objects[1].name) + " en case " + str(objects[1].position))
-print("Objet " + str(objects[2].name) + " en case " + str(objects[2].position))
+print("Case 'fin' : " + str(road[-1]))
+print("Objets " + str(len(objects)))
+print("Le tube est placé en case " + str(tube))
+print("L'aiguille est placée en case " + str(needle))
+print("L'ether est placée en case " + str(ether))
 
 macGyver.move("left")
 macGyver.move("right")
@@ -148,4 +61,16 @@ macGyver.move("down")
 macGyver.move("right")
 macGyver.move("right")
 macGyver.move("right")
-print("La liste 'objects' contient " + str(len(objects)) + " objets.")
+print("A la fin du jeu, la liste 'objects' contient " + str(len(objects)) + " objets.")
+if tube in objects:
+    print("Le tube n'a pas été ramassé.")
+else:
+    print("Le tube a été ramassé : bravo !")
+if needle in objects:
+    print("L'aiguille n'a pas été ramassée.")
+else:
+    print("L'aiguille a été ramassée : bravo !")
+if ether in objects:
+    print("L'ether n'a pas été ramassée.")
+else:
+    print("L'ether a été ramassé : bravo !")
