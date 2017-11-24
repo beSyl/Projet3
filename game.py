6c3fa6005c67c5f3bf1504be4654fb0d06543bf8
beSyl <32633621+beSyl@users.noreply.github.com>
2017-11-24 09:19:31 +0100

    Fixes #5

--- game.py
+++ game.py
@@ -6,35 +6,40 @@ from labyrinth import *
 from hero import *
 from my_pygame import *
 
-my_pygame = My_pygame()
 
-# PRINCIPAL LOOP
-play = 1
+def main():
+    my_pygame = My_pygame()
 
-while play == 1:
+    # PRINCIPAL LOOP
+    play = True
 
-    # Make a labyrinth from a file
-    labyrinth = Labyrinth(my_pygame)
-    # Create character
-    mac_gyver = Hero(labyrinth)
-    game = 1
+    while play:
 
-    # GAME LOOP
-    while game:
-        mac_gyver.activate(my_pygame)
-        my_pygame.refresh(labyrinth, mac_gyver)
+        # Make a labyrinth from a file
+        labyrinth = Labyrinth(my_pygame)
+        # Create character
+        mac_gyver = Hero(labyrinth)
+        game = True
 
-        # Pick up an object
-        if labyrinth.structure[mac_gyver.case_y][mac_gyver.case_x] in ['E', 'N', 'T']:
-            mac_gyver.cart += 1
-            labyrinth.structure[mac_gyver.case_y][mac_gyver.case_x] = ' '
-            if __name__ == "__main__":
-                print("Objet(s) dans le panier : " + str(mac_gyver.cart))
-                print(str(labyrinth.structure))
+        # GAME LOOP
+        while game:
+            mac_gyver.activate(my_pygame)
+            my_pygame.refresh(labyrinth, mac_gyver)
 
-        # End of game
-        if labyrinth.structure[mac_gyver.case_y][mac_gyver.case_x] == 'e':
-            mac_gyver.check_victory()
-            my_pygame.show_destiny(mac_gyver)
-            play = 0
-            game = 0
+            # Pick up an object
+            if labyrinth.structure[mac_gyver.case_y][mac_gyver.case_x] in ['E', 'N', 'T']:
+                mac_gyver.cart += 1
+                labyrinth.structure[mac_gyver.case_y][mac_gyver.case_x] = ' '
+                if __name__ == "__main__":
+                    print("Objet(s) dans le panier : " + str(mac_gyver.cart))
+                    print(str(labyrinth.structure))
+
+            # End of game
+            if labyrinth.structure[mac_gyver.case_y][mac_gyver.case_x] == 'e':
+                mac_gyver.check_victory()
+                my_pygame.show_destiny(mac_gyver)
+                play = False
+                game = False
+
+if __name__ == "__main__":
+    main()
